9ad394513f30e3b9d8c9c0fc4370095d47f45f50
diegorodriguezk <diegorodriguezkk@gmail.com>
2022-01-19 21:47:48 +0000

    time-line

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -10,37 +10,37 @@ const Timeline = () => {
   const [activeItem, setActiveItem] = useState(0);
   const carouselRef = useRef();
 
-  // const scroll = (node, left) => {
-  //   return node.scrollTo({ left, behavior: 'smooth' });
-  // }
+  const scroll = (node, left) => {
+    return node.scrollTo({ left, behavior: 'smooth' });
+  }
 
-  // const handleClick = (e, i) => {
-  //   e.preventDefault();
+  const handleClick = (e, i) => {
+    e.preventDefault();
 
-  //   if (carouselRef.current) {
-  //     const scrollLeft = Math.floor(carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length));
+    if (carouselRef.current) {
+      const scrollLeft = Math.floor(carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length));
       
-  //     scroll(carouselRef.current, scrollLeft);
-  //   }
-  // }
+      scroll(carouselRef.current, scrollLeft);
+    }
+  }
 
-  // const handleScroll = () => {
-  //   if (carouselRef.current) {
-  //     const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * TimeLineData.length);
+  const handleScroll = () => {
+    if (carouselRef.current) {
+      const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * TimeLineData.length);
 
-  //     setActiveItem(index);
-  //   }
-  // }
+      setActiveItem(index);
+    }
+  }
 
-  // // snap back to beginning of scroll when window is resized
-  // // avoids a bug where content is covered up if coming from smaller screen
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     scroll(carouselRef.current, 0);
-  //   }
+  // snap back to beginning of scroll when window is resized
+  // avoids a bug where content is covered up if coming from smaller screen
+  useEffect(() => {
+    const handleResize = () => {
+      scroll(carouselRef.current, 0);
+    }
 
-  //   window.addEventListener('resize', handleResize);
-  // }, []);
+    window.addEventListener('resize', handleResize);
+  }, []);
 
   return (
     <Section id='about'>
@@ -52,7 +52,15 @@ const Timeline = () => {
         <> 
           {TimeLineData.map((item, index) => {
             <CarouselMobileScrollNode key={index} final={index = TOTAL_CAROUSEL_COUNT - 1}>
-
+              <CarouselItem 
+              index={index}
+              id={'carousel__item-${index}'}
+              active={activeItem}
+              onClick={(e) => handleClick(e, index)} >
+                <CarouselItemTitle>
+                  {item.year}
+                </CarouselItemTitle>
+              </CarouselItem>
             </CarouselMobileScrollNode>
           })}
         </>
